1f3692f548ede288d9514fff5ff07e343b9f3678
Stephany Hoyos <154322758+stephanyhoy@users.noreply.github.com>
2024-05-04 22:41:53 +0200

    Delete Optica/marca_202403191249.sql

--- Optica/marca_202403191249.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO marca (nombre_marca,id_proveedor) VALUES
-	 ('Marca1',1),
-	 ('Marca2',2),
-	 ('Marca3',3),
-	 ('Gucci',1),
-	 ('Calvin Klein',2),
-	 ('Carolina Herrera',3),
-	 ('Milley & Marc',2);
