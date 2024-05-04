e9f7110c7fcf1753828cf283ba0f91cbefff99ce
Stephany Hoyos <154322758+stephanyhoy@users.noreply.github.com>
2024-05-04 22:42:04 +0200

    Delete Optica/proveedor_202403191240.sql

--- Optica/proveedor_202403191240.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO proveedor (nombre,apellido1,apellido2,direccion_calle,direccion_numero,direccion_piso,direccion_puerta,direccion_ciudad,direccion_codigo_postal,direccion_pais,telefono,fax,nif) VALUES
-	 ('José','Pinto','Ramírez','Av. Travesera de Dalt','18','3ro','6','Barcelona','08024','España','623456789','987654321','NIF123'),
-	 ('María','Galvez','Vásquez','Calle Providencia','45','1ro','A','Barcelona','54321','ESpaña','987654321','123456789','NIF456'),
-	 ('Miguel','López','Cueva','Torrent De L´Olla','69','2do','3','Barcelona','08035','España','635092769','963509278','NIF789');
